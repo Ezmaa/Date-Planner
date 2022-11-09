05b804fbb8aa7427c094b0573baaffcef77f633f
ezmaa <ezinmark@gmail.com>
2022-11-08 20:38:53 -0500

    added function to append drink information to page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const generateJokeEl = document.querySelector("#generate-joke");
 const generateRecipeEl = document.querySelector("#generate-recipe");
 const getRandomImage = document.querySelector("#random-image");
+const drinkName = document.querySelector('#drinkName');
+const ingredientList = document.querySelector('#ingredientsList');
 
 // function generateJoke() {
 
@@ -11,35 +13,40 @@ const getRandomImage = document.querySelector("#random-image");
 
 const drinkButtonVariable = document.getElementById('drinkButton');
 function generateRecipe() {
-  let randomUrl = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+    let randomUrl = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
 
-  fetch(randomUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      const drinkNumber = data.drinks[0].idDrink;
-      console.log(drinkNumber);
+    fetch(randomUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            const drinkNumber = data.drinks[0].idDrink;
+            console.log(drinkNumber);
 
-      // creates an array out of the object 
-      const drinkarr = Object.entries(data.drinks[0]);
-      console.log(drinkarr);
+            // creates an array out of the object 
+            const drinkarr = Object.entries(data.drinks[0]);
+            console.log(drinkarr);
 
-      // loops through the array to find ingredients used
-      for (let i = 0; i < drinkarr.length; i++) {
-        const key = drinkarr[i][0];
-        const value = drinkarr[i][1];
+            // loops through the array to find ingredients used
+            for (let i = 0; i < drinkarr.length; i++) {
+                const key = drinkarr[i][0];
+                const value = drinkarr[i][1];
 
+                if (key.toLowerCase().includes('ingredient') && value) {
+                    console.log(key, value);
+                    const recipe = value;
+                    console.log(recipe);
+                    drinkName.textContent = data.drinks[0].strDrink;
 
+                    const listEl = document.createElement('p');
+                    listEl.textContent = recipe;
+                    ingredientList.append(listEl);
+                    
+                };
 
-        if (key.toLowerCase().includes('ingredient') && value) {
-          console.log(key, value);
-
-        };
-
-      };
-    });
+            };
+        });
 }
 
 
